865bd04a0b2efb57f569bd3ea29e0402060c25d2
JuanQP <quinterosp.juan@gmail.com>
2023-10-22 16:57:59 -0300

    Generate VTT subtitles file from transcription

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -1,8 +1,23 @@
+import re
+
 from app.models import Audio
 from celery import shared_task
+from django.conf import settings
 from faster_whisper import WhisperModel
+from functools import reduce
 
 MODEL_SIZE = "medium"
+
+
+def seconds_to_str(t: int) -> str:
+    """function to convert floating point number of seconds to
+    hh:mm:ss,sss
+
+    https://stackoverflow.com/a/1384710/4792093
+    """
+    return "%02d:%02d:%02d,%03d" % \
+        reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],
+            [(round(t*1000),),1000,60,60])
 
 @shared_task
 def transcribe(audio_id: int):
@@ -14,8 +29,21 @@ def transcribe(audio_id: int):
     segments, _ = model.transcribe(audio.file.path, beam_size=5)
 
     audio.transcription = ""
-    for segment in segments:
-        audio.transcription += segment.text
+    audio_vtt_filename = re.sub(r"\.\w+$", ".vtt", audio.file.name.split("/")[-1])
+    audio_vtt_path = f"{settings.MEDIA_ROOT}/{audio.project.id}/{audio_vtt_filename}"
+    with open(audio_vtt_path, "w") as vtt_file:
+        vtt_file.write("WEBVTT\n\n")
+
+        for segment in segments:
+            text = segment.text
+            # Transcription
+            audio.transcription += text
+
+            # VTT subtitles file
+            start = seconds_to_str(segment.start)
+            end = seconds_to_str(segment.end)
+            vtt_file.write(f"{start} --> {end}\n")
+            vtt_file.write(f"{text.strip()}\n\n")
 
     audio.status = Audio.TranscriptionStatus.FINISHED
     audio.save()
